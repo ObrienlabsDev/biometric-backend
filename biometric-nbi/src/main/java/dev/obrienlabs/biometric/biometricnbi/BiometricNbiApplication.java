0323b296e6b52c9ab467a537c83bd4891a9d07f6
Michael O'Brien <michael@obrienlabs.org>
2023-05-13 16:12:25 -0400

    #1 - initial spring boot template

---
+++ biometric-nbi/src/main/java/dev/obrienlabs/biometric/biometricnbi/BiometricNbiApplication.java
@@ -0,0 +1,13 @@
+package dev.obrienlabs.biometric.biometricnbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiometricNbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiometricNbiApplication.class, args);
+	}
+
+}
---
+++ biometric-nbi/src/test/java/dev/obrienlabs/biometric/biometricnbi/BiometricNbiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.obrienlabs.biometric.biometricnbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiometricNbiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
