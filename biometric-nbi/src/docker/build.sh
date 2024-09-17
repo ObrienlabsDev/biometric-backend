cde51199d04708bd5cc8347007b416fe70d90c6e
Michael OBrien <michael@obrienlabs.org>
2024-09-17 00:59:52 +0000

    #6 - adjust build for ia64

--- biometric-nbi/src/docker/build.sh
+++ biometric-nbi/src/docker/build.sh
@@ -5,8 +5,8 @@
 # Michael O'Brien
 # docker login -u username with token before running - to push
 
-#TAG=0.0.1-ia64
-TAG=0.0.1-arm
+TAG=0.0.1-ia64
+#TAG=0.0.1-arm
 
 BUILD_ID=10001
 BUILD_DIR=builds
@@ -71,9 +71,9 @@ cd ../../src/docker
 ## --network="host"
 #export GOOGLE_APPLICATION_CREDENTIALS=~/.config/gcloud/application_default_credentials.json
 #echo "docker run -d -p 8888:8080 --name biometric-nbi -e GOOGLE_APPLICATION_CREDENTIALS=application_default_credentials.json obrienlabs/biometric-nbi:0.0.1-arm"
-docker run -d -p 8888:8080 --name biometric-nbi obrienlabs/$CONTAINER_IMAGE:$TAG
-echo "curl -X GET \"http://192.168.0.105:8888/nbi/api/getGps?ac=0&action=u2&al=0&arx=0&ary=0&arz=0&be=0&grx=0&gry=0&grz=0&gsx=0&gsy=0&gsz=0&hr1=0&hr2=0&hrd1=0&hrd2=0&hu=0&lax=0&lay=0&laz=0&lg=0&li=0&lt=0&mfx=0&mfy=0&mfz=0&p=0&pr=0&px=0&rvx=0&rvy=0&rvz=0&s=0&te=0&ts=0&u=202408040&up=0\" -H \"accept: */*\""
+docker run -d -p 8888:8080 --network="mysql" --name biometric-nbi obrienlabs/$CONTAINER_IMAGE:$TAG
+echo "curl -X GET \"http://192.168.0.105:8888/nbi/api/getGps?ac=0&action=u2&arx=0&ary=0&arz=0&be=0&grx=0&gry=0&grz=0&gsx=0&gsy=0&gsz=0&hr1=0&hr2=0&hrd1=0&hrd2=0&hu=0&lax=0&lay=0&laz=0&li=0&lg=-75.940427&lt=45.343839&al=095.706317&mfx=0&mfy=0&mfz=0&p=0&pr=0&px=0&rvx=0&rvy=0&rvz=0&s=0&te=0&ts=0&u=202408040&up=0\" -H \"accept: */*\""
 echo "sleep 10 then check containers"
 sleep 10
 docker ps
-curl -X GET "http://192.168.0.105:8888/nbi/api/getGps?ac=0&action=u2&al=0&arx=0&ary=0&arz=0&be=0&grx=0&gry=0&grz=0&gsx=0&gsy=0&gsz=0&hr1=0&hr2=0&hrd1=0&hrd2=0&hu=0&lax=0&lay=0&laz=0&lg=0&li=0&lt=0&mfx=0&mfy=0&mfz=0&p=0&pr=0&px=0&rvx=0&rvy=0&rvz=0&s=0&te=0&ts=0&u=202408040&up=0" -H "accept: */*"
+curl -X GET "http://192.168.0.105:8888/nbi/api/getGps?ac=0&action=u2&arx=0&ary=0&arz=0&be=0&grx=0&gry=0&grz=0&gsx=0&gsy=0&gsz=0&hr1=0&hr2=0&hrd1=0&hrd2=0&hu=0&lax=0&lay=0&laz=0&li=0&lg=-75.940427&lt=45.343839&al=095.706317&mfx=0&mfy=0&mfz=0&p=0&pr=0&px=0&rvx=0&rvy=0&rvz=0&s=0&te=0&ts=0&u=202408042&up=0" -H "accept: */*"
