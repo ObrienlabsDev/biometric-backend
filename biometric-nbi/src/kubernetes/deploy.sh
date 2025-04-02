ce4bcd89f05b7d4bc48d69ae1a04fa29a4d120a1
Michael O'Brien <michael@obrienlabs.org>
2025-04-01 21:43:40 -0400

    #21 - script deploy start

---
+++ biometric-nbi/src/kubernetes/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+kubectl apply -f mysql-namespace.yaml
+kubectl apply -f mysql-secret.yaml
+kubectl apply -f mysql-storage.yaml
+kubectl apply -f mysql-secret.yaml
+kubectl apply -f mysql-deployment.yaml
+kubectl apply -f mysql-service.yaml
+
+
+#kubectl get namespaces
+#kubectl get secrets -n mysql
+#kubectl describe secret mysql-secret -n mysql
+#kubectl get pv -n mysql
+#kubectl get pvc -n mysql
+#kubectl get pods -n mysql
+#kubectl get service -n mysql
+   
+#kubectl exec --stdin --tty mysql-7cf66456c-hhqxr -- /bin/bash
+#kubectl exec --stdin --tty mysql-7cf66456c-hhqxr -n mysql -- /bin/bash
+ 
