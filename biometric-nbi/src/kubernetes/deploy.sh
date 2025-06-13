25b47b5906d09d5aecb8175802b0e1541fa38be2
Michael O'Brien <michael@obrienlabs.org>
2025-06-13 10:37:01 -0400

    #6 - adjust deploy/undeploy scripts

--- biometric-nbi/src/kubernetes/deploy.sh
+++ biometric-nbi/src/kubernetes/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 kubectl apply -f mysql-namespace.yaml
-kubectl apply -f mysql-secret.yaml
 kubectl apply -f mysql-storage.yaml
 kubectl apply -f mysql-secret.yaml
 kubectl apply -f mysql-deployment.yaml
